cc33ae46b068c380e222c9082f9fc780107447b5
isabellechan <70530997+isabellechan@users.noreply.github.com>
2022-05-12 10:15:25 -0400

    fixed variable type

--- aesthetics.py
+++ aesthetics.py
@@ -1,15 +1,15 @@
 import pygame
 import random
 
-# Margins
-MARGIN_LEFT = 230
-MARGIN_TOP = 150
+# window size
+width = 800
+height = 600
 
-# WINDOW SIZE
-WIDTH = 800
-HEIGHT = 600
+#margins
+left_margin = 230
+top_margin = 150
 
-# COLORS
+#colors 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (110, 110, 110)
@@ -17,15 +17,11 @@ GREEN = (0, 255, 0)
 LIGHT_GREEN = (0, 120, 0)
 RED = (255, 0, 0)
 LIGHT_RED = (120, 0, 0)
-DARK_GREEN = (69,139,0)
+DARK_GREEN = (0,100,0)
 PURPLE = (171,130,255)
 
-
-# The type of suit
+#card values
 suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
-
-# The type of card
 cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-
-# The card value
 cards_values = {"A": 1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":11, "Q":12, "K":13}
+
